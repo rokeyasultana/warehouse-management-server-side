3e4650493116e3b3551dbce1078700ec1f24905a
jakiasultana2022 <rokeyasbd@gmail.com>
2023-04-12 20:24:35 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -83,20 +83,13 @@ app.put("/product/:id", async (req, res) => {
 
 
  // Delete items
-        
+ 
         app.delete('/product/:id', async (req, res) => {
           const id = req.params.id;
           const query = { _id: ObjectId(id) };
           const result = await productCollection.deleteOne(query);
           res.send(result);
       });
- 
-
-      
- 
-
-
-
 
     }
     finally{
